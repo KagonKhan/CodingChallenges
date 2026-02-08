f3e1447977e06472ae1f2ffb5bd97e06987ddc9c
KagonKhan <s.szynk98@gmail.com>
2026-02-08 16:50:08 +0100

    why is everything so cancerous

---
+++ CodingChallenges/json-parser/json.cpp
@@ -0,0 +1,18 @@
+#include "json.hpp"
+
+#include <string>
+#include <iostream>
+
+#include "spdlog/spdlog.h"
+
+class JSON {
+public:
+void parse(std::string string) {
+
+}
+};
+
+
+int main() { 
+    
+}
